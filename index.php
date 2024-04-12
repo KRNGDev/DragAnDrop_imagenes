29705c173c6aa443dbed03407bb9f87626baf8d2
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-04-12 07:21:51 +0200

    sum

--- index.php
+++ index.php
@@ -9,42 +9,49 @@
     <link rel="stylesheet" type="text/css" href="estilos.css" />
     <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 
-
-
 </head>
 <header>
 
-
+    <div class="logo">
+        <img src="Imagenes/661796c87635a.png" alt="Logo">
+        <h3>Programado | Desarrollador</h3>
+    </div>
 
     <h1 align="center">Carga tu imagen</h1>
+
+   
+        <a href="https://albertolopezmartinez.top/" type="submit" class="button" ">Inicio</a>
+   
 
 </header>
 
 <body>
 
-    <div class="container">
+    
 
-        <h1 align="center">Hola gente que tal</h1>
+                <h1 align="center">Hola gente que tal</h1>
 
-        <form action="Guardar.php" class="dropzone" id="dropzoneFrom">
+                <form action="Guardar.php" class="dropzone" id="dropzoneFrom">
 
-        </form>
+                </form>
 
-        <div class="boton">
-            <button type="submit" class="btn btn-info" id="submit-all">Cargar</button>
-        </div>
+                
+                <button type="submit" class="btn btn-info" id="submit-all">Cargar</button>
+                
 
 
 
-    </div>
-    <div id="preview"></div>
+    
 
+   
+        <div id="preview"></div>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
     <script src="app.js"></script>
 
 
 
-</body>
+    </body>
 
 </html>
